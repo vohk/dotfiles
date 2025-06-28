9da3493c3cace5318c0aec574c43a00dc4e91852
vohk <8020903+vohk@users.noreply.github.com>
2025-06-28 13:02:18 -0700

    Update .config/helix/config.toml Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,4 +5,13 @@ end
 set -U fish_greeting
 set -gx EDITOR hx
 
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
